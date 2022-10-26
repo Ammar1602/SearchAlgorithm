165f91c1b0fa2e0024c9caf3d8aad43cbaf83534
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-10-26 16:07:43 +0700

    add int i and int n

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,6 +8,17 @@ namespace SearchAlgorithm
 {
     class Program
     {
+        //Aaray to be searched
+        int[] arr = new int[20];
+        //Number of element in the array 
+        int n;
+        //Get the number of elements to show in the array
+        int i;
+
+        public void input()
+        {
+            
+        }
         static void Main(string[] args)
         {
         }
